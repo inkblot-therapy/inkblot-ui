56823da6454e0cd36259f3116f639b41569e2afa
Ben Verna-Asals <benjamin.asals@gmail.com>
2019-01-23 14:00:02 -0500

    Fix down arrow svg

--- src/svg/Down.tsx
+++ src/svg/Down.tsx
@@ -1,14 +1,17 @@
 import * as React from "react";
 
 const SvgDown = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg width={9} height={4} {...props}>
-    <path
-      fill="none"
-      stroke="#2E5FCA"
-      strokeLinecap="round"
-      strokeWidth={2}
-      d="M8.439.386c-2.532 2.328-3.89 3.493-4.073 3.493-.183 0-1.58-1.165-4.19-3.493"
-    />
+  <svg width={13} height={11} {...props}>
+    <path fill="none" d="M-1-1h15v13H-1z" />
+    <g>
+      <path
+        d="M10.564 3.697c-2.532 2.33-3.89 3.494-4.074 3.494-.183 0-1.58-1.164-4.19-3.493"
+        strokeWidth={2}
+        strokeLinecap="round"
+        stroke="#2E5FCA"
+        fill="none"
+      />
+    </g>
   </svg>
 );
 
