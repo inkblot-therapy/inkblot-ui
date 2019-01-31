deeb18265cc7d53f017a31d9bb15fe71ff5cc533
Simon Park <simon@inkblottherapy.com>
2019-01-31 16:04:36 -0500

    create InkblotTheme

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,15 +1,8 @@
 import * as React from 'react';
-import { ButtonProps } from './components/Button/Button';
-import { DropdownProps } from './components/Dropdown/Dropdown';
-declare const ThemeButton: (props: ButtonProps) => JSX.Element;
-declare const ThemeDropdown: (props: DropdownProps) => JSX.Element;
-declare const InkblotTheme: React.StatelessComponent<{}>;
+export declare const InkblotTheme: React.StatelessComponent<{}>;
 export { default as BookingCalendar } from "./components/BookingCalendar";
 export { default as Button } from "./components/Button";
 export { default as Dropdown } from "./components/Dropdown";
 export { default as Modal } from "./components/Modal";
 export { default as SearchDropdown } from "./components/SearchDropdown";
 export { default as TextInput } from "./components/TextInput";
-export { ThemeButton };
-export { ThemeDropdown };
-export { InkblotTheme };
--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,18 @@
 import * as React from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from './theme';
-import Button from './components/Button';
-import { ButtonProps } from './components/Button/Button'
-import Dropdown from "./components/Dropdown";
-import { DropdownProps } from './components/Dropdown/Dropdown'
 
-const ThemeButton = (props: ButtonProps) => (
-  <ThemeProvider theme={theme}>
-    <Button {...props} />
-  </ThemeProvider>
-);
-
-const ThemeDropdown = (props: DropdownProps) => (
-  <ThemeProvider theme={theme}>
-    <Dropdown {...props} />
-  </ThemeProvider>
-);
-
-const InkblotTheme: React.StatelessComponent<{}> = ({children}) => (
+export const InkblotTheme: React.StatelessComponent<{}> = ({children}) => (
   <ThemeProvider theme={theme}>
     <div>
       {children}
     </div>
   </ThemeProvider>
 )
+
 export { default as BookingCalendar } from "./components/BookingCalendar";
 export { default as Button } from "./components/Button";
 export { default as Dropdown } from "./components/Dropdown";
 export { default as Modal } from "./components/Modal";
 export { default as SearchDropdown } from "./components/SearchDropdown";
 export { default as TextInput } from "./components/TextInput";
-export { ThemeButton };
-export { ThemeDropdown };
-export { InkblotTheme };
