f0f728dbaca90cf4df28adfef3ea95cbfad3a165
Simon Park <simon@inkblottherapy.com>
2019-01-31 14:41:56 -0500

    added Themebutton

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,11 @@
+/// <reference types="react" />
+declare const ThemeButton: ({ children }: {
+    children: any;
+}) => JSX.Element;
 export { default as BookingCalendar } from "./components/BookingCalendar";
 export { default as Button } from "./components/Button";
 export { default as Dropdown } from "./components/Dropdown";
 export { default as Modal } from "./components/Modal";
 export { default as SearchDropdown } from "./components/SearchDropdown";
 export { default as TextInput } from "./components/TextInput";
-export { default as InkblotTheme } from './utils/themed';
+export { ThemeButton };
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,17 @@
+import * as React from 'react';
+import { ThemeProvider } from 'styled-components';
+import theme from './theme';
+
+const ThemeButton = ({children}) => (
+  <ThemeProvider theme={theme}>
+    {children}
+  </ThemeProvider>
+);
+
 export { default as BookingCalendar } from "./components/BookingCalendar";
 export { default as Button } from "./components/Button";
 export { default as Dropdown } from "./components/Dropdown";
 export { default as Modal } from "./components/Modal";
 export { default as SearchDropdown } from "./components/SearchDropdown";
 export { default as TextInput } from "./components/TextInput";
-export { default as InkblotTheme } from './utils/themed';
+export { ThemeButton };
