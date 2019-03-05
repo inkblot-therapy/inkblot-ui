ceee434d908be41fce42422804f0abd926c22203
etiotan <etiotan@gmail.com>
2019-03-05 14:36:07 -0500

    build libary

--- lib/utils/wInfo.d.ts
+++ lib/utils/wInfo.d.ts
@@ -1,2 +1,2 @@
 /// <reference types="react" />
-export declare const wInfo: (text?: string | undefined) => (storyFn: import("@storybook/react").RenderFunction) => (context?: object | undefined) => import("react").ReactElement<import("@storybook/addon-info").WrapStoryProps, string | ((props: any) => import("react").ReactElement<any, string | any | (new (props: any) => import("react").Component<any, any, any>)> | null) | (new (props: any) => import("react").Component<any, any, any>)>;
+export declare const wInfo: (text?: string | undefined) => (storyFn: import("storybook__react").RenderFunction) => (context?: object | undefined) => import("react").ReactElement<import("storybook__addon-info").WrapStoryProps>;
